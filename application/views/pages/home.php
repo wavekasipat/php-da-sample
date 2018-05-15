6f68cf07c542066ddc693c5c418aac9983b2702f
Kasipat <wavekasipat@gmail.com>
2018-05-15 16:04:29 +0700

    up home.php

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -13,7 +13,7 @@
             <img data-src="<?php echo base_url("assets/images/sb_1.jpg");?>" alt="900x500" src="<?php echo base_url("assets/images/banner-1.jpg");?>">
             <div class="carousel-caption">
                 <h3> Varial Kick Flip </h3>
-                <p> Do a Kick Flip or some serious Technical Skating on Motion Board 9000</p>
+                <p> Do a Kick Flip or some serious Technical Skating on Motion Board 111111</p>
             </div>
         </div>
         <div class="item" onclick="location.href='<?php echo base_url();?>product'">
